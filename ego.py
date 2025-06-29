338a69cb448e170397c7c9788d0017fb8c4291f5
Zcaic <462845148@qq.com>
2025-06-29 22:04:03 +0800

    test cdf

--- ego.py
+++ ego.py
@@ -2,8 +2,11 @@ import numpy as np
 from smt.applications import EGO 
 from smt.surrogate_models import KRG 
 
+
+class Criterion():
+    def __init__(self):
+        pass
+    def 
 class MEGO(EGO):
     def optimize(self, fun):
         return super().optimize(fun)
-
-KRG().nx
